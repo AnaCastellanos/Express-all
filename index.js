1e35fa453b126ba8508f424568c9bbfc9da8a2d6
Ana Castellanos <ana.lec15@gmail.com>
2018-04-16 21:04:52 -0500

    Express

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const express = require('express'); //Instanciamos express
 const app = express(); //Express nos da una función inicial que se asigna a una constante.
 
-app.get('/', (req, res) => {  //Require: Toda la informaciñon que el navegador esta pidiendo.
+app.get('/', (req, res) => {  //Require: Toda la información que el navegador esta pidiendo.
 	res.end('Hola Mundo'); //Response: Respuesta por parte del servidor.
-});//Cuando el servidor reciba una petición de un navegador, ejecuta la función.
+});
 
 app.listen(3000, () => {
 	console.log('Servidor funcionando en el puerto 3000');
